9788519a04783c2364e1d96f9a24ea7a233fc40a
motakabbirmorshed <morsheddolar1990@gmail.com>
2023-04-09 09:40:00 +0600

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'phgov/logtracker',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '50d38701c29a446726d2871d4731cafbd61bcb8b',
+        'type' => 'package',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phgov/logtracker' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '50d38701c29a446726d2871d4731cafbd61bcb8b',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
